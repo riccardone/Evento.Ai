1b7e833ddd4da675c8f9da9081479540c0525211
Riccardo Di Nuzzo <riccardo.d@xml-int.com>
2025-01-10 13:31:01 +0000

    training the model

--- src/Evento.Ai.Chatter/ModelTrainer.cs
+++ src/Evento.Ai.Chatter/ModelTrainer.cs
@@ -1,9 +1,45 @@
-﻿namespace Evento.Ai.Chatter;
+﻿using OpenAI;
+
+namespace Evento.Ai.Chatter;
 
 public class ModelTrainer
 {
     public static IEnumerable<OpenAI.Chat.Message> DiscoverInfoAboutValidationSchema(string data)
     {
-        throw new NotImplementedException();
+        return new List<OpenAI.Chat.Message>
+        {
+            new(Role.System, @"
+In the context of a distributed application where incoming messages need to be validated, you are going to create a json schema based on a user story and some acceptance criteria. You need to define in the schema all the properties that are necessary to implement the requirement. Try to use flat properties instead of hierarchies if possible. Each property has a data type and some constraints to limit security risks. You will need to create a JSON response with the Json schema. Each schema must have a CorrelationId string property and TenantId string property. These two mandatory properties have minLength=3 and maxLength=256. In the schema, additionalProperties is set to false.
+User: As a interested user I want to accept an invitation So I can join the company on the platform
+Assistant: {
+    ""$schema"": ""https://json-schema.org/draft/2020-12/schema"",
+    ""type"": ""object"",
+    ""properties"": {
+        ""CorrelationId"": {
+            ""type"": ""string"",
+            ""minLength"": 3,
+            ""maxLength"": 256
+        },
+        ""UserId"": {
+            ""type"": ""string"",
+            ""minLength"": 3,
+            ""maxLength"": 256
+        },
+        ""TenantId"": {
+            ""type"": ""string""
+            """"minLength"""": 3,
+            """"maxLength"""": 256
+        }
+    },
+    ""required"": [
+        ""CorrelationId"",
+        ""UserId"",
+        ""TenantId""
+    ],
+    ""additionalProperties"": false
+}
+"),
+            new(Role.User, data)
+        };
     }
 }
